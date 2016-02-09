8006e74d459ffc87a11d94e34551db1fb5b8872e
gistop <gistop@163.com>
2016-02-09 09:22:17 +0800

    mod

--- ae/LeftForm.Designer.cs
+++ ae/LeftForm.Designer.cs
@@ -28,10 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LeftForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.splitContainer1.Panel1MinSize = 5;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.axTOCControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(292, 273);
+            this.splitContainer1.SplitterDistance = 96;
+            this.splitContainer1.TabIndex = 0;
             // 
             // axTOCControl1
             // 
@@ -39,25 +62,47 @@
             this.axTOCControl1.Location = new System.Drawing.Point(0, 0);
             this.axTOCControl1.Name = "axTOCControl1";
             this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
-            this.axTOCControl1.Size = new System.Drawing.Size(292, 273);
+            this.axTOCControl1.Size = new System.Drawing.Size(292, 173);
             this.axTOCControl1.TabIndex = 0;
+            this.axTOCControl1.OnMouseDown += new ESRI.ArcGIS.Controls.ITOCControlEvents_Ax_OnMouseDownEventHandler(this.axTOCControl1_OnMouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(77, 26);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(76, 22);
+            this.toolStripMenuItem2.Text = "1";
             // 
             // LeftForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.axTOCControl1);
+            this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Name = "LeftForm";
             this.Text = "leftform";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer1;
         private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+
     }
 }
--- ae/LeftForm.cs
+++ ae/LeftForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,10 +15,69 @@ namespace ae
 {
     public partial class LeftForm : DockContent
     {
+        //为了配合toc右键菜单,初始化ITocControl接口方法中HitTest的各个参数
+        private esriTOCControlItem pTocItem = esriTOCControlItem.esriTOCControlItemNone;
+        private IBasicMap pMap = null;
+        private ILayer pLayer = null;
+        private object pother = null;
+        private object pindex = null;
+        
+        
         public LeftForm()
         {
             InitializeComponent();
             Global.toc = this.axTOCControl1;
+            splitContainer1.SplitterDistance = 10;
+        }
+
+        private void axTOCControl1_OnMouseDown(object sender, ESRI.ArcGIS.Controls.ITOCControlEvents_OnMouseDownEvent e)
+        {
+            if (e.button == 2)
+            {
+                //判断所选菜单的类型
+                axTOCControl1.HitTest(e.x, e.y, ref pTocItem, ref pMap, ref pLayer, ref pother, ref pindex);
+                //axMapControl1.CustomProperty = layer;
+
+                //弹出右键菜单
+                /*if (item == esriTOCControlItem.esriTOCControlItemMap)
+                    pMenuMap.PopupMenu(e.x, e.y, m_tocControl.hWnd);
+                if (item == esriTOCControlItem.esriTOCControlItemLayer)
+                    pMenuLayer.PopupMenu(e.x, e.y, m_tocControl.hWnd);*/
+                //菜单的创建
+                //pMenuMap = new ToolbarMenuClass();
+                //pMenuLayer = new ContextMenuStrip();
+                if (pTocItem == esriTOCControlItem.esriTOCControlItemLayer)
+                {
+                    //pFLayer = pLayer as IFeatureLayer;
+                    //pFC = pFLayer.FeatureClass;
+
+                    //pFeatureLayer = pLayer as IFeatureLayer;
+                    /*   pfeatureclass = pFeatureLayer.FeatureClass;
+                       SymbolToolStripMenuItem.DropDownItems.Clear();
+                       for (int i = 0; i < pfeatureclass.Fields.FieldCount; i++)
+                       {
+                           SymbolToolStripMenuItem.DropDownItems.Add(pfeatureclass.Fields.get_Field(i).Name, null, new EventHandler(this.symbolshow_Clicked)); }
+                
+                   */
+
+                    contextMenuStrip1.Show(axTOCControl1, new System.Drawing.Point(e.x, e.y));
+
+                    //m_menuLayer.AddItem(new frmAttribute(axMapControl1, pLayer), -1, 2, true, esriCommandStyles.esriCommandStyleTextOnly);
+
+                    //m_menuLayer = new ToolbarMenuClass();
+                    //m_menuLayer.AddItem(new OpenAttributeTable(pLayer), -1, 0, true, esriCommandStyles.esriCommandStyleTextOnly);
+                    //动态添加图层标注的Command到图层右键菜单
+                    //m_menuLayer.AddItem(new LabelLayerCmd(pLayer, m_mapControl), -1, 1, false, esriCommandStyles.esriCommandStyleTextOnly);
+                    //m_menuLayer.AddItem(new LabelLayerCmd(pLayer), -1, 1, false, esriCommandStyles.esriCommandStyleTextOnly);
+
+                    //弹出图层右键菜单m_mapControl
+                    //m_menuLayer.SetHook(m_mapControl);
+                    //m_menuLayer.PopupMenu(e.x, e.y, axTOCControl1.hWnd);
+                    // 注意顺序不能颠倒
+                    //m_menuLayer.Remove(1);
+                    //m_menuLayer.Remove(0)
+                };
+            }
         }
     }
 }
